045e1fa1bc147400bbb0b118361cd6312cd06652
johnmart19 <johnivan19999@gmail.com>
2023-02-26 12:48:21 +0200

    Apollo: Import Xiaomi TouchFeature service

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -148,6 +148,7 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/vintf/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/vintf/device_framework_matrix.xml
