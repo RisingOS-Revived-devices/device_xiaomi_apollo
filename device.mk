9245c8882ca1023db023ce2f2c1e928fcfab5a17
LuK1337 <priv.luk@gmail.com>
2024-12-25 23:42:18 +0100

    Apollo: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643

--- device.mk
+++ device.mk
@@ -398,6 +398,7 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
+    NcmTetheringOverlay \
     WifiResCommon \
     wpa_supplicant \
     wpa_supplicant.conf
